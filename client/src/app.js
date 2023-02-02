e9f4ecc328305ac6884e3bb27835dc99d8a61b28
quangtv <p-quangtv@nissho-vn.com>
2023-02-02 17:00:09 +0700

    using the useStage hook actually tells the OWL Observer to watch change inside the state view native Proxy Object

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,23 @@
 const {Component, mount} = owl;
+const { useState } = owl.hooks;
 // owl  version = '1.4.11';
 const {xml} = owl.tags;
 const {whenReady} = owl.utils;
 
 // Owl Components
 class App extends Component {
-    // static template = xml`<div>Hello world</div>`;
+    state = useState({ counterValue: 0 });
+    alert_mss() {
+        this.state.counterValue += 1;
+    }
 }
 
-App.template = xml`<div>Hello world!</div>`;
+App.template = xml`
+<div>Hello owl! 
+<br />
+<button t-on-click="alert_mss">Click me!</button>
+<p>Counter value: <t t-raw="state.counterValue"/></p> 
+</div>`;
 
 // Setup code
 function setup() {
